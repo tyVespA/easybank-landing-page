f7ff81022a8ba934b304efc8b06f39fa645dcb44
tyVespA <migliavacca.marco08@gmail.com>
2024-12-15 11:52:31 +0100

    Add new articles, style

--- components/ArticlesSection/ArticlesSection.js
+++ components/ArticlesSection/ArticlesSection.js
@@ -21,6 +21,33 @@ export default function ArticlesSection() {
             The world is getting smaller and we’re becoming more mobile. So why
             should you be forced to only receive money in a single …
           </ArticleCard>
+          <ArticleCard
+            imageName={restaurant}
+            alt={"restaurant"}
+            author={"Wilson Hutton"}
+            title={"Treat yourself without worrying about money"}
+          >
+            Our simple budgeting feature allows you to separate out your
+            spending and set realistic limits each month. That means you …
+          </ArticleCard>
+          <ArticleCard
+            imageName={plane}
+            alt={"plane"}
+            author={"Wilson Hutton"}
+            title={"Take your Easybank card wherever you go"}
+          >
+            We want you to enjoy your travels. This is why we don’t charge any
+            fees on purchases while you’re abroad. We’ll even show you …
+          </ArticleCard>
+          <ArticleCard
+            imageName={confetti}
+            alt={"confetti"}
+            author={"Claire Robinson"}
+            title={"Our invite-only Beta accounts are now live!"}
+          >
+            After a lot of hard work by the whole team, we’re excited to launch
+            our closed beta. It’s easy to request an invite through the site ...
+          </ArticleCard>
         </div>
       </div>
     </section>
